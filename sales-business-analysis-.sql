91ce20ad4fc2bb641ef5d7cfaefd48de64fea95c
tombacchini <tombacchini@gmail.com>
2026-04-04 18:28:08 +0200

    sales-business-analysis-.sql

--- sales-business-analysis-.sql
+++ sales-business-analysis-.sql
@@ -1,19 +1,10 @@
--- =====================================
--- SALES ANALYSIS PROJECT
--- PostgreSQL
--- =====================================
 
-
--- =====================================
 -- 1. Chiffre d'affaires total
--- =====================================
 
 SELECT SUM(net_revenue) AS total_revenue
 FROM sales_financial;
 
--- =====================================
 -- 2. Chiffre d'affaires par mois 
--- =====================================
 
 SELECT DATE_TRUNC('month', sale_date) AS month,
        SUM(net_revenue) AS revenue
@@ -21,9 +12,7 @@ FROM sales_financial
 GROUP BY month
 ORDER BY month;
 
--- =====================================
 -- 3. Evolution mensuelle du chiffre d'affaires par rapport au mois précédent 
--- =====================================
 
 WITH monthly_revenue AS (
     SELECT
@@ -39,18 +28,16 @@ SELECT
     revenue - LAG(revenue) OVER (ORDER BY month) AS revenue_change
 FROM monthly_revenue
 ORDER BY month;
--- =====================================
+
 -- 4. Produits les moins rentables 
--- =====================================
 
 SELECT product_name, ROUND(AVG(margin_pct),2) AS avg_margin
 FROM sales_financial
 GROUP BY product_name
 ORDER BY avg_margin ASC
 LIMIT 5;
--- =====================================
+
 -- 5. Top 5 Clients 
--- =====================================
 
 SELECT client_name, SUM(net_revenue) AS revenue
 FROM sales_financial 
@@ -58,10 +45,7 @@ GROUP BY client_name
 ORDER BY revenue DESC 
 LIMIT 5;
 
-
--- =====================================
 -- 6. Clients les plus performants par rapport à la moyenne
--- =====================================
 
 WITH client_revenue AS (
     SELECT
@@ -80,9 +64,7 @@ WHERE revenue > (
 )
 ORDER BY revenue DESC;
 
--- =====================================
 -- 7. Part du chiffre d'affaires par région
--- ===================================== 
 
 SELECT
     region,
@@ -95,9 +77,7 @@ FROM sales_financial
 GROUP BY region
 ORDER BY revenue DESC;
 
--- =====================================
 -- 8. Performance par catégorie
--- ===================================== 
  	
 SELECT
     category,
